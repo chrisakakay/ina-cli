49e6a1576eb59253006cf37fefd778bea0661226
Krisztian Nagy <chrisakakay@gmail.com>
2017-01-31 00:00:18 +0100

    base

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const ina   = require('ina');
+const chalk = require('chalk');
+const names = process.argv.slice(2);
+const Spnnr = require('spnnr');
+
+function check(name) {
+    return new Promise((resolve, reject) => {
+        let spinner = new Spnnr(`${chalk.gray('Looking for')} ${chalk.white.bold(name)}`);
+
+        ina.check(name).then((info) => {
+            if (info.exists === true) {
+                spinner.stop(`${chalk.red.bold('×')} ${chalk.white.bold(name)} ${chalk.gray('is unavailable')}`);
+            } else {
+                spinner.stop(`${chalk.green.bold('√')} ${chalk.white.bold(name)} ${chalk.gray('is available')}`);
+            }
+
+            resolve();
+        });
+    });
+}
+
+if (names.length === 0) {
+    /*eslint no-console: 0*/
+    console.log('Hey you forgot to specify the name(s) you are looking for.');
+    process.exit(1);
+}
+
+let flow = Promise.resolve();
+
+names.forEach((name) => {
+    flow.then(() => check(name));
+});
