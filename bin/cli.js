ba66025be0ce889f9f24785452d6a6dbaab9ed57
Krisztian Nagy <Krisztian_Nagy@epam.com>
2017-02-01 17:28:49 +0100

    improved promise handling

--- bin/cli.js
+++ bin/cli.js
@@ -1,22 +1,40 @@
 #!/usr/bin/env node
 'use strict';
+/*eslint no-console: 0*/
 
-const ina       = require('ina');
-const chalk     = require('chalk');
-const Spnnr     = require('spnnr');
-const homedir   = require('os').homedir();
-const argv      = require('minimist')(process.argv.slice(2), { boolean: ['list', 'save']});
-const names     = argv._;
+const ina           = require('ina');
+const chalk         = require('chalk');
+const Spnnr         = require('spnnr');
+const fs            = require('fs');
+const path          = require('path');
+const homedir       = path.join(require('os').homedir(), '.ina/');
+const homefile      = path.join(homedir + 'packages.json');
+const argv          = require('minimist')(process.argv.slice(2), { boolean: ['list', 'save']});
+const names         = argv._;
+
+const clr           = chalk.gray;
+const clrR          = chalk.red.bold;
+const clrG          = chalk.green.bold;
+const clrW          = chalk.white.bold;
+
+const namesToSave   = [];
+let   promises      = [];
+
+function createDefaultHomefile() {
+    fs.writeFileSync(homefile, JSON.stringify({ available: [] }), 'utf-8');
+    console.log(clr('Cache created for saving package names. Have fun!'));
+}
 
 function check(name) {
     return new Promise((resolve, reject) => {
-        let spinner = new Spnnr(`${chalk.gray('Looking for')} ${chalk.white.bold(name)}`);
+        let spinner = new Spnnr(`${clr('Looking for')} ${clrW(name)}`);
 
         ina.check(name).then((info) => {
             if (info.exists === true) {
-                spinner.stop(`${chalk.red.bold('×')} ${chalk.white.bold(name)} ${chalk.gray('is unavailable')}`);
+                spinner.stop(`${clrR('×')} ${clrW(name)} ${clr('is unavailable')}`);
             } else {
-                spinner.stop(`${chalk.green.bold('√')} ${chalk.white.bold(name)} ${chalk.gray('is available')}`);
+                spinner.stop(`${clrG('√')} ${clrW(name)} ${clr('is available')}`);
+                if (argv.save) { namesToSave.push(name); };
             }
 
             resolve();
@@ -24,14 +42,40 @@ function check(name) {
     });
 }
 
+if (argv.list) {
+    if (!fs.existsSync(homedir)) {
+        fs.mkdirSync(homedir);
+        createDefaultHomefile();
+    } else if (fs.existsSync(homefile)) {
+        let data = require(homefile);
+
+        if (data.available.length > 0) {
+            data.available.forEach((name) => {
+                console.log(`  ${clrW(name)}`);
+            });
+        } else {
+            console.log(clr('You have not saved any package name yet.'));
+        }
+    } else {
+        createDefaultHomefile();
+    }
+
+    process.exit(0);
+}
+
 if (names.length === 0) {
-    /*eslint no-console: 0*/
-    console.log('Hey you forgot to specify the name(s) you are looking for.');
+    console.log(clr('Hey you forgot to specify the name(s) you are looking for.'));
     process.exit(1);
 }
 
-let flow = Promise.resolve();
-
 names.forEach((name) => {
-    flow.then(() => check(name));
+    promises.push(check(name));
+});
+
+Promise.all(promises).then(() => {
+    let data = require(homefile);
+
+    data.available = Array.from(new Set(data.available.concat(namesToSave)));
+
+    fs.writeFileSync(homefile, JSON.stringify(data), 'utf-8');
 });
