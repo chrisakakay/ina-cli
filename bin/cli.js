8e97148653a2bee66fe9e5db8e763f7653bc26ba
Krisztian Nagy <Krisztian_Nagy@epam.com>
2017-02-02 13:04:57 +0100

    added extra info

--- bin/cli.js
+++ bin/cli.js
@@ -16,6 +16,7 @@ const clr           = chalk.gray;
 const clrR          = chalk.red.bold;
 const clrG          = chalk.green.bold;
 const clrW          = chalk.white.bold;
+const clrY          = chalk.yellow.bold;
 
 const namesToSave   = [];
 let   promises      = [];
@@ -25,15 +26,39 @@ function createDefaultHomefile() {
     console.log(clr('Cache created for saving package names. Have fun!'));
 }
 
+function getVersionString(version) {
+    return clrY('@' + version);
+}
+
+function getDaysPassed(firstStr) {
+    const now   = new Date();
+    const first = new Date(firstStr);
+    const a     = Date.UTC(first.getFullYear(), first.getMonth(), first.getDate());
+    const b     = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate());
+    const days  = Math.floor((b - a) / 86400000);
+
+    return 'updated ' + days + ' days ago';
+}
+
 function check(name) {
     return new Promise((resolve, reject) => {
         let spinner = new Spnnr(`${clr('Looking for')} ${clrW(name)}`);
 
         ina.check(name).then((info) => {
             if (info.exists === true) {
-                spinner.stop(`${clrR('×')} ${clrW(name)} ${clr('is unavailable')}`);
+                spinner.stop([
+                    clrR('×'),
+                    clrW(name + '@' +info.version),
+                    clr('is unavailable,'),
+                    clr(getDaysPassed(info.modified)),
+                ].join(' '));
             } else {
-                spinner.stop(`${clrG('√')} ${clrW(name)} ${clr('is available')}`);
+                spinner.stop([
+                    clrG('√'),
+                    clrW(name),
+                    clr('is available')
+                ].join(' '));
+
                 if (argv.save) { namesToSave.push(name); };
             }
 
